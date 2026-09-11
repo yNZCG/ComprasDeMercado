fc555e233e0bf6dd9db3ab19f6ca232cbce0b292
Guilherme Alphonsus Crelier <72390798+yNZCG@users.noreply.github.com>
2026-09-11 15:30:56 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'nota-de-compras-v1';
+const ARQUIVOS_PARA_CACHE = [
+  './nota-de-compras.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS_PARA_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(nomes.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((resposta) => {
+      if (resposta) return resposta;
+      return fetch(event.request)
+        .then((rede) => {
+          const copia = rede.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copia));
+          return rede;
+        })
+        .catch(() => resposta);
+    })
+  );
+});
